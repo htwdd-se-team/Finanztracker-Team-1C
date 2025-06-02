ab30b0e18de1c1497ca7bbdee12f4bad483dbf8a
eliasblume <mail@elias-blu.me>
2025-06-02 12:11:08 +0200

    refactor: migrate Next.js configuration from .mjs to .ts format

--- src/frontend/next.config.ts
+++ src/frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true
+  },
+  output: 'export',
+  pageExtensions: ['tsx', 'mdx'],
+  productionBrowserSourceMaps: false,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '<API_URL>',
+  },
 };
 
 export default nextConfig;
