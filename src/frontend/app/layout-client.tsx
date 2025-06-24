7a97e0d83fe0f1dce0b99f9c9f1fa8e47bea5307
Christoph Prasser <christoph.prasser2@stud.htw-dresden.de>
2025-06-24 17:59:01 +0200

    Bugfix - Tableiste immer sichtbar

--- src/frontend/app/layout-client.tsx
+++ src/frontend/app/layout-client.tsx
@@ -24,41 +24,40 @@ export default function LayoutClient({ children }: { children: ReactNode }) {
             {/* Mobile */}
             <div id="mobile" className="block sm:hidden flex-col min-h-screen pb-20">
                 {children}
-            </div>
-
-            {/* Funktionsleiste unten */}
-            <div className="fixed inset-x-0 bottom-0 border-t bg-background z-50">
-                <div className="relative">
-                <Tabs defaultValue="overview" onValueChange={(val) => setActiveTab(val)}>
-                    <TabsList className="grid grid-cols-5 w-full h-14">
-                    <TabsTrigger value="overview" onClick={() => router.push("/overview")} className="flex flex-col items-center justify-center">
-                        <Home className="size-7" />
-                    </TabsTrigger>
-                    <TabsTrigger value="charts" onClick={() => router.push("/graphs")} className="flex flex-col items-center justify-center">
-                        <ChartNoAxesCombined className="size-7" />
-                    </TabsTrigger>
-                    {/* Platzhalter für mittleren Tab */}
-                    <div />
-                    <TabsTrigger value="table" onClick={() => router.push("/table")} className="flex flex-col items-center justify-center">
-                        <TableProperties className="size-7" />
-                    </TabsTrigger>
-                    <TabsTrigger value="profile" onClick={() => router.push("/profile")} className="flex flex-col items-center justify-center">
-                        <UserRoundCog className="size-7" />
-                    </TabsTrigger>
-                    </TabsList>
-                </Tabs>
-                {/* Floating Button */}
-                <button
-                    className="
-                    absolute left-1/2 -translate-x-1/2 -top-6
-                    w-16 h-16 rounded-full bg-primary text-white
-                    flex items-center justify-center shadow-lg z-10
-                    hover:bg-primary/90 transition
-                    "
-                    // OnClick Handler
-                >
-                    <SquarePlus className="size-8" />
-                </button>
+                {/* Funktionsleiste unten */}
+                <div className="fixed inset-x-0 bottom-0 border-t bg-background z-50">
+                    <div className="relative">
+                    <Tabs defaultValue="overview" onValueChange={(val) => setActiveTab(val)}>
+                        <TabsList className="grid grid-cols-5 w-full h-14">
+                        <TabsTrigger value="overview" onClick={() => router.push("/overview")} className="flex flex-col items-center justify-center">
+                            <Home className="size-7" />
+                        </TabsTrigger>
+                        <TabsTrigger value="charts" onClick={() => router.push("/graphs")} className="flex flex-col items-center justify-center">
+                            <ChartNoAxesCombined className="size-7" />
+                        </TabsTrigger>
+                        {/* Platzhalter für mittleren Tab */}
+                        <div />
+                        <TabsTrigger value="table" onClick={() => router.push("/table")} className="flex flex-col items-center justify-center">
+                            <TableProperties className="size-7" />
+                        </TabsTrigger>
+                        <TabsTrigger value="profile" onClick={() => router.push("/profile")} className="flex flex-col items-center justify-center">
+                            <UserRoundCog className="size-7" />
+                        </TabsTrigger>
+                        </TabsList>
+                    </Tabs>
+                    {/* Floating Button */}
+                    <button
+                        className="
+                        absolute left-1/2 -translate-x-1/2 -top-6
+                        w-16 h-16 rounded-full bg-primary text-white
+                        flex items-center justify-center shadow-lg z-10
+                        hover:bg-primary/90 transition
+                        "
+                        // OnClick Handler
+                    >
+                        <SquarePlus className="size-8" />
+                    </button>
+                    </div>
                 </div>
             </div>
 
