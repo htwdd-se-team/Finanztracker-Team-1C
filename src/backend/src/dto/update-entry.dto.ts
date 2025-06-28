693469e14c601a8870fd9b8c4e2c642e6a2585b4
Tuan Khang Pham <tuankhang.pham@stud.htw-dresden.de>
2025-06-28 21:47:33 +0200

    feat: added option again to edit createdAt

--- src/backend/src/dto/update-entry.dto.ts
+++ src/backend/src/dto/update-entry.dto.ts
@@ -1,7 +1,5 @@
-import { PartialType, OmitType } from "@nestjs/swagger";
+import { PartialType } from "@nestjs/swagger";
 
 import { CreateEntryDto } from "./create-entry.dto";
 
-export class UpdateEntryDto extends PartialType(
-  OmitType(CreateEntryDto, ["createdAt"] as const),
-) {}
+export class UpdateEntryDto extends PartialType(CreateEntryDto) {}
