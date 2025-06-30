7a34330397c0c68cfcc00ba3cb2b25cb31983b03
eliasblume <mail@elias-blu.me>
2025-06-30 19:18:02 +0200

    refactor: update favicon generation and manifest configuration

--- src/frontend/components/nav/finapp-logo.tsx
+++ src/frontend/components/nav/finapp-logo.tsx
@@ -1,13 +1,23 @@
 import { cn } from '@/lib/utils'
-import { TrendingUp } from 'lucide-react'
+import Image from 'next/image'
 
 const FinAppLogo = ({ className }: { className?: string }) => {
   return (
-    <div className={cn('flex justify-center items-center mb-8', className)}>
+    <div
+      className={cn(
+        'flex justify-center items-center mb-8 text-left',
+        className
+      )}
+    >
       <div className="relative flex items-center space-x-2">
-        <div className="relative flex justify-center items-center bg-primary shadow-lg rounded-lg w-10 h-10">
-          <TrendingUp className="w-6 h-6 text-background" />
-        </div>
+        <Image
+          src="/images/logo-high-res.png"
+          alt="FinApp Logo"
+          width={48}
+          height={48}
+          className="relative flex justify-center items-center shadow-lg rounded-lg object-contain"
+        />
+
         <div className="flex flex-col">
           <span className="font-bold text-foreground text-2xl tracking-tight">
             Fin<span className="text-primary">App</span>
