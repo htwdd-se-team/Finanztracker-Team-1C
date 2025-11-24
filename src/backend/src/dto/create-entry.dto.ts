968838744e5a98793079e707f64e50537bb39912
eliasblume <mail@elias-blu.me>
2025-11-24 13:24:20 +0100

    feat: add recurring date validation decorator

--- src/backend/src/dto/create-entry.dto.ts
+++ src/backend/src/dto/create-entry.dto.ts
@@ -2,20 +2,27 @@ import { ApiPropertyOptional, OmitType } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsDate, IsOptional } from "class-validator";
 
+import { IsRecurringDateValid } from "../decorators/recurring-date-validator";
+
 import { EntryResponseDto } from "./entry-response.dto";
 
+/**
+ * Create entry DTO
+ */
 export class CreateEntryDto extends OmitType(EntryResponseDto, [
   "id",
   "createdAt",
   "transactionId",
   "recurringDisabled",
 ]) {
-  @ApiPropertyOptional({
-    description:
-      "Creation timestamp. If not provided, defaults to the current date and time.",
-  })
+  /**
+   * Creation timestamp. If not provided, defaults to the current date and time.
+   * For recurring entries, the creation date cannot be more than 30 days in the past.
+   */
+  @ApiPropertyOptional()
   @IsOptional()
   @IsDate()
+  @IsRecurringDateValid()
   @Type(() => Date)
   createdAt?: Date;
 }
