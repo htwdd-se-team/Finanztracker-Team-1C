76b51b41769b98a4dd400721dc66e0bafba09371
eliasblume <mail@elias-blu.me>
2025-12-12 00:31:32 +0100

    fix: add ESLint disable comments for multer storage configuration in AppModule

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -49,6 +49,7 @@ import { JwtStrategy } from "./strategies";
     ScheduleModule.forRoot(),
     MulterModule.register({
       dest: undefined,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call -- multer is not typed
       storage: memoryStorage(),
     }),
   ],
